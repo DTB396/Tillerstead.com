b2952e5b70cacd1f69e77bad616478c6633bcb33
Tillerstead Dev <dev@tillerstead.com>
2026-01-06 16:04:11 -0500

    Stop tracking vendor folder

--- vendor/gems/jekyll/lib/jekyll.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "jekyll/version"
-require_relative "jekyll/cli"
--- vendor/gems/jekyll/lib/jekyll/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Jekyll
-  VERSION = "999.0.0"
-end
